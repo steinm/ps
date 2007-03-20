ed7598a7b24cac259d6a862a28d8aadc3d2a6148
Uwe Steinmann <uwe@steinmann.cx>
2007-03-20 10:18:29 +0000

    - new example to demostrate patterns

---
+++ examples/pattern.php
@@ -0,0 +1,34 @@
+<?php
+$ps = ps_new();
+
+if (!ps_open_file($ps, "pattern.ps")) {
+	print "Cannot open PostScript file\n";
+	exit;
+}
+
+ps_set_parameter($ps, "warning", "true");
+ps_set_info($ps, "Creator", "pattern.php");
+ps_set_info($ps, "Author", "Uwe Steinmann");
+ps_set_info($ps, "Title", "Pattern example");
+
+
+$pspattern = ps_begin_pattern($ps, 10.0, 10.0, 10.0, 10.0, 1);
+ps_setlinewidth($ps, 0.2);
+ps_setcolor($ps, "stroke", "rgb", 0.0, 0.0, 1.0, 0.0);
+ps_moveto($ps, 0, 0);
+ps_lineto($ps, 7, 7);
+ps_stroke($ps);
+ps_moveto($ps, 0, 7);
+ps_lineto($ps, 7, 0);
+ps_stroke($ps);
+ps_end_pattern($ps);
+
+ps_begin_page($ps, 596, 842);
+ps_setcolor($ps, "both", "pattern", $pspattern, 0.0, 0.0, 0.0);
+ps_rect($ps, 50, 400, 200, 200);
+ps_fill($ps);
+ps_end_page($ps);
+
+ps_close($ps);
+ps_delete($ps);
+?>
